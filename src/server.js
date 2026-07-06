b1e23f6507b21e9870a31f24efeb9453d25fe2f2
shchoung <raphael31004@naver.com>
2026-07-07 00:14:24 +0900

    Remove route for serving index.html
    Removed route handler for serving index.html file.

--- src/server.js
+++ src/server.js
@@ -3,11 +3,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, '../public')));
 
-// 2. 루트 경로('/')로 접속했을 때 public/index.html 파일을 보여줌
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
