94ce3d15b91ad2d3561abd4600c0767e960506cd
shchoung <raphael31004@naver.com>
2026-07-07 00:12:40 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,8 +3,6 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// 1. public 폴더 안의 CSS, 이미지, JS 등 정적 파일들을 브라우저에 보낼 수 있도록 설정
-app.use(express.static(path.join(__dirname, 'public')));
 
 // 2. 루트 경로('/')로 접속했을 때 public/index.html 파일을 보여줌
 app.get('/', (req, res) => {
